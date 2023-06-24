5f1d168fb914527a189f8e7ed89820ae60afc204
EphraimShikanga <ephraimshikanga@gmail.com>
2023-06-24 04:50:48 -0700

    create_file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-file {'/tmp/scchool':
-  ensure  => 'file',
-  mode    => '0744',
+file { '/tmp/scchool':
+  ensure  => present,
+  mode    => '0744,
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
 }
+
