1b4cdafc1056bb45d489b2df77597fb26502ec19
Ephraim Shikanga <ephraimshikanga@gmail.com>
2023-08-18 14:57:53 +0300

    recurse_it

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,13 +9,11 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=""):
     """Returns a list containing the titles of all hot articles"""
-    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
-                                                                after)
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(
+        subreddit, after)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        results = response.json().get("data")
-        count += results.get("dist")
         for post in response.json().get('data').get('children'):
             hot_list.append(post.get('data').get('title'))
         after = response.json().get('data').get('after')
