620dc1b991ce4dae0a65cc404c339dbc860633c6
EphraimShikanga <ephraimshikanga@gmail.com>
2023-08-18 12:04:09 +0300

    how_many_subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and returns the number of subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    return 0
