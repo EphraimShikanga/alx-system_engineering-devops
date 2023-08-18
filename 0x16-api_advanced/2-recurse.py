205d18c7493ff747cd467fbc111de6e56cf12640
Ephraim Shikanga <ephraimshikanga@gmail.com>
2023-08-18 14:56:30 +0300

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,23 +9,16 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=""):
     """Returns a list containing the titles of all hot articles"""
-    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
+                                                                after)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers,params=params,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         results = response.json().get("data")
         count += results.get("dist")
-
-        for post in result.get('children'):
+        for post in response.json().get('data').get('children'):
             hot_list.append(post.get('data').get('title'))
-    
-        after = result.get('data').get('after')
+        after = response.json().get('data').get('after')
         if after is None:
             return hot_list
         return recurse(subreddit, hot_list, after)
